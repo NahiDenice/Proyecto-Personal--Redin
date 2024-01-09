f5a440f2eb7f5b878dbd2de51717b8c7d04da5cb
NahiDenice <nahiara.alegre@hotmail.com>
2024-01-09 08:58:48 -0300

    first commit

---
+++ Recursos Didácticos/redin/src/main/java/com/redin/redin/RedinApplication.java
@@ -0,0 +1,13 @@
+package com.redin.redin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedinApplication.class, args);
+	}
+
+}
---
+++ Recursos Didácticos/redin/src/test/java/com/redin/redin/RedinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redin.redin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
